4187abb09f3e301b65a7c1d95f0ff8b1d3c5ef56
R P <ashwinkumar.rp@cognizant.com>
2020-01-27 16:26:23 +0530

    Icon change for WhatsApp

--- src/ShareVerse/ShareVerse.js
+++ src/ShareVerse/ShareVerse.js
@@ -6,7 +6,7 @@ import './ShareVerse.css';
 const Shareverse=(props)=>{
     return(<div className="modal">
        <div className="pop_buttons">
-            {props.isVerse===true ?<WhatsappShareButton  url={props.data.Verse + "\n" + ":-" + props.chapter + props.chapterNo +":"+ props.verseNo +"\n"+ "https://bibleapp-front-end.herokuapp.com/"} ><WhatsappIcon size={25} round={true} logoFillColor="white"/></WhatsappShareButton>:null}
+            {props.isVerse===true ?<WhatsappShareButton  url={props.data.Verse + "\n" + ":-" + props.chapter + props.chapterNo +":"+ props.verseNo +"\n"+ "https://bibleapp-front-end.herokuapp.com/"} ><span className="fa fa-share-alt share"></span></WhatsappShareButton>:null}
             <span onClick={props.close} className="close">&times;</span>
        </div>
         <div className="img_share_container">
