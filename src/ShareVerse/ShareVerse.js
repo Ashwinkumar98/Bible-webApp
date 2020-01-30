c953b0e4660c43c04837003acf6600482e9c9dcf
R P <ashwinkumar.rp@cognizant.com>
2020-01-30 15:01:39 +0530

    URL CHANGE

--- src/ShareVerse/ShareVerse.js
+++ src/ShareVerse/ShareVerse.js
@@ -6,7 +6,7 @@ import './ShareVerse.css';
 const Shareverse=(props)=>{
     return(<div className="modal">
        <div className="pop_buttons">
-            {props.isVerse===true ?<WhatsappShareButton  url={props.data.Verse + "\n" + ":-" + props.chapter + props.chapterNo +":"+ props.verseNo +"\n"+ `https://bibleapp-front-end.herokuapp.com/`} ><span className="fa fa-share-alt share"></span></WhatsappShareButton>:null}
+            {props.isVerse===true ?<WhatsappShareButton  url={props.data.Verse + "\n" + ":-" + props.chapter + props.chapterNo +":"+ props.verseNo +"\n"+ `https://tamilaudiobible.herokuapp.com/`} ><span className="fa fa-share-alt share"></span></WhatsappShareButton>:null}
             <span onClick={props.close} className="close">&times;</span>
        </div>
         <div className="img_share_container">
